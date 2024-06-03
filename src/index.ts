5c3ed6b0b72c6fe87f4285da584b2b1bfed96bcf
marylandabu <marylandabu@gmail.com>
2024-06-03 15:07:40 -0400

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-export {
-  degToCompass,
-  getChartOptions,
-  formatDate,
-  formatTime,
-  formatDayAndTime,
-} from "./utils/utils";
+export * from "./utils/utils";
 
-export { useWeatherHighlights } from "./utils/useWeatherHighlights.ts/useWeatherHighlights";
+export * from "./utils/useWeatherHighlights.ts/useWeatherHighlights";
 
-export {
-  useAccessToken,
-  useActions,
-  useUserData,
-  getAccessToken,
-  getActions,
-  getUserData,
-} from "./store/authStore";
+export * from "./store/authStore";
 
-export type { AuthState, UserData } from "./store/types";
+export type * from "./store/types";
