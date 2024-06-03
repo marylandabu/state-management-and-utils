639fd9a17a8cc82f1e3f9958f84a58afdc6418f6
marylandabu <marylandabu@gmail.com>
2024-06-03 14:59:52 -0400

    updated config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import { dts } from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
@@ -29,13 +29,13 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
-      postcss,
+      postcss(),
     ],
     external: ["react", "react-dom"],
   },
   {
     input: "src/index.ts",
     output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [dts.default()],
+    plugins: [dts()],
   },
 ];
