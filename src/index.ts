3d94fdec9b82571a4fc8babbda7a37bb377d6daf
marylandabu <marylandabu@gmail.com>
2024-06-05 15:44:57 -0400

    update index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "./utils/utils";
 
 export * from "./utils/hooks/useWeatherHighlights.ts/useWeatherHighlights";
 
+export * from "./utils/hooks/useGetUser";
+
 export * from "./store/authStore";
 
 export type * from "./store/types";
